1e65737e5f06909612a48234051065f1c9066843
Alexander Haider <alexander_haider@hotmail.com>
2020-09-28 09:19:07 +0200

    Make wait for script reusable

--- .github/workflows/waitForFrontend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Waiting frontend to launch on 4200..."
-
-while ! nc -z localhost 4200; do
-  sleep 0.1 # wait for 1/10 of the second before check again
-done
-
-echo "Frontend launched"
