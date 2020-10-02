3da1655080815882d6fee92e22289f8d85a5b545
Alexander Haider <alexander_haider@hotmail.com>
2020-10-02 14:29:25 +0200

    Fix cypress tests

--- frontend/cypress/integration/login.spec.js
+++ frontend/cypress/integration/login.spec.js
@@ -7,14 +7,12 @@ describe('login', () => {
   })
 
   it('show error on wrong credentials', () => {
-    cy.mockGraphQl('token', {
-      errors: {
-        "message": "failed get token",
-        "path": [
-          "token"
-        ]
-      }});
-    cy.mockGraphQl('me', {data: { me: { id: "asdf"}}});
+    cy.route({
+      method: 'POST',
+      url: '**/user/api/public/token',
+      status: 400,
+      response:{},
+    }).as('postToken');
 
     cy.visit('');
     cy.location('pathname').should('eq', '/login');
@@ -22,26 +20,36 @@ describe('login', () => {
     cy.get('input[placeholder="Password"]').type('HaidelBert');
     cy.get('input[placeholder="Password"]').type('HaidelBert');
     cy.get('button').click();
+    cy.wait('@postToken');
 
     cy.contains('Username or password wrong!').should('be.visible');
   });
 
   it('successfully logs user in', () => {
-    cy.mockGraphQl('token', {
-      "data": {
-        "token": {
-          "accessToken": "asdf"
-        }
-      }
-    });
-    cy.mockGraphQl('me', { data: { me: { id: "asdf" }}});
-
+    cy.route({
+      method: 'POST',
+      url: '**/user/api/public/token',
+      response:{
+        accessToken: 'asdf'
+      },
+    }).as('postToken');
+    cy.route({
+      method: 'GET',
+      url: '**/user/api/protected/me',
+      response:{
+        id: 1,
+        username: 'HaidelBert',
+        email: 'alexander_haider@hotmail.com',
+      },
+    }).as('getUserDetails');
     cy.visit('');
     cy.location('pathname').should('eq', '/login');
     cy.get('input[placeholder="Username"]').type('HaidelBert');
     cy.get('input[placeholder="Password"]').type('HaidelBert');
     cy.get('input[placeholder="Password"]').type('HaidelBert');
     cy.get('button').click();
+    cy.wait('@postToken');
+    cy.wait('@getUserDetails');
 
     cy.location('pathname').should('eq', '/');
     cy.contains('Dashboard').should('be.visible');
