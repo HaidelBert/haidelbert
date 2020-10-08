8c5dc2d016bc61c1207d56d136112ca4b2b12e98
Alexander Haider <alexander_haider@hotmail.com>
2020-10-08 18:01:16 +0200

    Add afs project

---
+++ annual-financial-statements/src/main/kotlin/io/haidelbertcom/example/annualfinancialstatements/AnnualFinancialStatementsApplication.kt
@@ -0,0 +1,11 @@
+package io.haidelbertcom.example.annualfinancialstatements
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AnnualFinancialStatementsApplication
+
+fun main(args: Array<String>) {
+	runApplication<AnnualFinancialStatementsApplication>(*args)
+}
---
+++ annual-financial-statements/src/test/kotlin/io/haidelbertcom/example/annualfinancialstatements/AnnualFinancialStatementsApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.haidelbertcom.example.annualfinancialstatements
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AnnualFinancialStatementsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
