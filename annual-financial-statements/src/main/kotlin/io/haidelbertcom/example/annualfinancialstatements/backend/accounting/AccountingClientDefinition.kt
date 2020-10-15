2a8fb82194aa60b95a63223af40724321ec22e73
Alexander Haider <alexander_haider@hotmail.com>
2020-10-15 11:13:07 +0200

    fix config

--- annual-financial-statements/src/main/kotlin/io/haidelbertcom/example/annualfinancialstatements/backend/accounting/AccountingClientDefinition.kt
+++ annual-financial-statements/src/main/kotlin/io/haidelbertcom/example/annualfinancialstatements/backend/accounting/AccountingClientDefinition.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
 
-@FeignClient(value = "accountingClientDefinition", url = "\${accountingServiceUrl}")
+@FeignClient(value = "accountingClientDefinition", url = "\${accounting.serviceUrl}")
 interface AccountingClientDefinition {
 
     @RequestMapping(method = [RequestMethod.GET], value = ["/accounting/api/internal/"])
--- annual-financial-statements/src/main/kotlin/io/haidelbertcom/example/annualfinancialstatements/config/ServiceCredentials.kt
+++ annual-financial-statements/src/main/kotlin/io/haidelbertcom/example/annualfinancialstatements/config/ServiceCredentials.kt
@@ -4,5 +4,5 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
-class ServiceCredentials(@Value("\${accounting.credentials.username}") val serviceName: String,
-                         @Value("\${accounting.credentials.password}") val servicePassword: String)
+class ServiceCredentials(@Value("\${afs.credentials.username}") val serviceName: String,
+                         @Value("\${afs.credentials.password}") val servicePassword: String)
