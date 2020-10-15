8cb20782252658be0eb5fd6c5c6f79f624b95041
Alexander Haider <alexander_haider@hotmail.com>
2020-10-15 13:05:24 +0200

    refactoring

--- annual-financial-statements/src/main/kotlin/io/haidelbertcom/example/annualfinancialstatements/backend/registerOfAssets/RegisterOfAssetsClientDefinition.kt
+++ annual-financial-statements/src/main/kotlin/io/haidelbertcom/example/annualfinancialstatements/backend/registerOfAssets/RegisterOfAssetsClientDefinition.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
 
-@FeignClient(value = "registerOfAssetsClientDefinition", url = "\${registerOfAssetsServiceUrl}")
+@FeignClient(value = "registerOfAssetsClientDefinition", url = "\${register-of-assets.serviceUrl}")
 interface RegisterOfAssetsClientDefinition {
     @RequestMapping(method = [RequestMethod.GET], value = ["/register-of-assets/api/internal/year-depreciations"])
     fun getYearDepreciationInternal(
