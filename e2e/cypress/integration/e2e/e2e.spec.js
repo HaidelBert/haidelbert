9a30cd1fe228a80e06ede04bbf7c9cb1a3a51a79
Alexander Haider <alexander_haider@hotmail.com>
2020-10-20 13:16:15 +0200

    stabilize cypress tests

--- e2e/cypress/integration/e2e/e2e.spec.js
+++ e2e/cypress/integration/e2e/e2e.spec.js
@@ -5,8 +5,9 @@ describe('Main Usecase', () => {
         cy.server();
         cy.route('POST', 'user/api/public/token').as('token');
         cy.route('GET', 'user/api/protected/me').as('me');
-        cy.route('PATCH', '/vat/api/protected/pre-registration/*').as('patchRecord');
-        cy.route('POST', '/vat/api/protected/pre-registration').as('postRecord');
+        cy.route('PATCH', '/vat/api/protected/pre-registration/*').as('patchPreRegistration');
+        cy.route('POST', '/vat/api/protected/pre-registration').as('postPreRegistration');
+        cy.route('GET', '/vat/api/protected/pre-registration*').as('getPreRegistration');
         cy.visit("");
         cy.location('pathname').should('eq', '/login');
         cy.get('input[placeholder="Username"]').type('HaidelBert');
@@ -29,6 +30,7 @@ describe('Main Usecase', () => {
 
         [1, 2, 3, 4].forEach(quarter => {
             cy.get('button[cy-data="new-pre-registration"]').click();
+            cy.wait(10);
             cy.get('input[formcontrolname="year"]').eq(0).should('be.visible');
             cy.get('input[formcontrolname="year"]').eq(0).type(lastYear.toString(10));
             cy.get('nz-select[formcontrolname="interval"]').eq(0).click()
@@ -36,16 +38,18 @@ describe('Main Usecase', () => {
             cy.get('nz-select[formcontrolname="quarter"]').eq(0).click()
             cy.get('.ant-select-item-option-content').contains(quarter).click();
             cy.contains('Speichern').click();
-            cy.wait("@postRecord");
+            cy.wait("@postPreRegistration");
         });
         cy.get('nz-select[cy-data="year-filter"]').click();
         cy.contains(lastYear).click();
+        cy.wait("@getPreRegistration");
 
         cy.get('tbody[cy-data="pre-registration-table"]>tr')
             .each(($el, index, $list) => {
                 cy.wait(10);
                 cy.get('button[cy-data="mark-tx-authority-done"]').eq(0).click();
-                cy.wait("@patchRecord");
+                cy.wait("@patchPreRegistration");
+                cy.wait("@getPreRegistration");
             });
     });
 });
