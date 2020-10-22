afb284013a8893ddeea6b7b83faff9719952da27
Alexander Haider <alexander_haider@hotmail.com>
2020-10-22 11:05:20 +0200

    Add more waiting

--- infra/ci/waitForEndpoint.sh
+++ infra/ci/waitForEndpoint.sh
@@ -5,5 +5,4 @@ timeout -s TERM 30 bash -c \
 'while [[ "$(curl -s -o /dev/null -L -w ''%{http_code}'' ${0})" != "200" ]];\
 do echo "Waiting for ${0}" && sleep 2;\
 done' ${1}
-echo "OK!"
-curl -I $1
+echo "${1} OK!"
